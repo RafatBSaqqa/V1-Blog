627e600f1fb97c6dcfcb56ca5b86976cea2b5eb7
RafatBSaqqa <raafat.saqqa@gmail.com>
2024-04-22 17:21:19 +0300

    Create router

---
+++ client/src/layouts/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Outlet} from "react-router-dom"
+import Navbar from "../../components/Navbar"
+export default function Main() {
+  return (
+    <div>
+        <header>
+            <Navbar/>
+        </header>
+        <main>
+            <Outlet/>
+        </main>
+    </div>
+  )
+}
